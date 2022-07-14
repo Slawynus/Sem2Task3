18f2c031a14d88b803424f6a54fb0d688436cebe
Slawynus <108095246+Slawynus@users.noreply.github.com>
2022-07-14 16:30:19 +0300

    Sem2
    Task3

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("введите число");
+int numD = Convert.ToInt32(Console.ReadLine());
+
+if (numD ==6 || numD == 7)
+{
+    Console.WriteLine("выходной");
+    return;
+}
+if (numD < 6 && numD > 0)
+{
+    Console.WriteLine("рабочий день");
+    return;
+}
+if (numD > 7 || numD < 1)
+{
+    Console.WriteLine("нет такого дня");
+    return;
+}
